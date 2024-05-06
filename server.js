c801105d02be9001bf8ea6ba64b637d1fe4ecba5
Tony Stark <apurificato5680@gmai.com>
2024-05-06 09:33:53 -0400

    removed /Develop folder, fixed file paths, now testing application again

--- server.js
+++ server.js
@@ -2,22 +2,23 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-const notesRouter = require('./Develop/routes/note_routes.js');
+const notesRouter = require('./routes/note_routes.js');
 
 const PORT = process.env.PORT || 3333;
 
-app.use(express.static(path.join(__dirname, 'Develop', 'public')));
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.json());
 
 app.use('/api/notes', notesRouter);
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/Develop/public/index.html'));
+  res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
 app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, 'Develop/public/notes.html'));
+  res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
 app.listen(PORT, () => {
